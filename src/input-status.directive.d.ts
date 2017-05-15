1c2f29f45808fa6027153bf9dae31cfb113f6116
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2017-05-15 08:22:39 +0700

    Remove generated files

--- src/input-status.directive.d.ts
+++
@@ -1,6 +0,0 @@
-import { InputService } from "./model";
-export declare class InputStatusDirective {
-    private _inputService;
-    constructor(_inputService: InputService);
-    readonly invalid: boolean;
-}
